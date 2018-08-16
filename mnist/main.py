ec0d88042e32eedd5513587c1f887cbac2abaf32
TiagoLeite <tiago.tmleite@gmail.com>
2018-08-16 18:43:49 -0300

    mlp model

--- mnist/main.py
+++ mnist/main.py
@@ -8,12 +8,24 @@ x = tf.placeholder(dtype=tf.float32, shape=[None, 784])
 y = tf.placeholder(dtype=tf.float32, shape=[None, 10])
 
 # variables (parameters)
-w = tf.Variable(tf.truncated_normal(shape=[784, 10], stddev=0.1))
-b = tf.Variable(tf.constant(0.1, shape=[10]))
+w1 = tf.Variable(tf.random_normal(shape=[784, 200], stddev=0.1))
+b1 = tf.Variable(tf.constant(0.1, shape=[200]))
+
+w2 = tf.Variable(tf.random_normal(shape=[200, 100], stddev=0.1))
+b2 = tf.Variable(tf.constant(0.1, shape=[100]))
+
+w3 = tf.Variable(tf.random_normal(shape=[100, 10], stddev=0.1))
+b3 = tf.Variable(tf.constant(0.1, shape=[10]))
 
 # model:
-z = tf.matmul(x, w) + b
-y_ = tf.nn.softmax(z)
+z1 = tf.matmul(x, w1) + b1
+y1 = tf.nn.relu(z1)
+
+z2 = tf.matmul(y1, w2) + b2
+y2 = tf.nn.relu(z2)
+
+z3 = tf.matmul(y2, w3) + b3
+y_ = tf.nn.softmax(z3)
 
 error = tf.reduce_sum(-(y*tf.log(y_ + 0.00001)))
 
@@ -27,7 +39,7 @@ with tf.Session() as sess:
 
     sess.run(tf.global_variables_initializer())
 
-    for k in range(10000):
+    for k in range(20000):
 
         x_batch, y_batch = mnist.train.next_batch(100)
 
