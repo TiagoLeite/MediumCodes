245eab475967a69d9502c3a4ef2340b4290afbc8
TiagoLeite <tiago.tmleite@gmail.com>
2018-08-16 16:45:45 -0300

    model

--- mnist/main.py
+++ mnist/main.py
@@ -3,39 +3,48 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
+# placeholders:
 x = tf.placeholder(dtype=tf.float32, shape=[None, 784])
 y = tf.placeholder(dtype=tf.float32, shape=[None, 10])
 
+# variables (parameters)
 w = tf.Variable(tf.truncated_normal(shape=[784, 10], stddev=0.1))
 b = tf.Variable(tf.constant(0.1, shape=[10]))
 
+# model:
 z = tf.matmul(x, w) + b
 y_ = tf.nn.softmax(z)
 
-# error = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_))
+error = tf.reduce_sum(-(y*tf.log(y_ + 0.00001)))
 
-error = tf.reduce_sum(tf.squared_difference(y, y_))
+prediction = tf.equal(tf.argmax(y, axis=1), tf.argmax(y_, axis=1))
+acc = tf.reduce_mean(tf.cast(prediction, tf.float32))
 
-acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y, axis=1), tf.argmax(y_, axis=1)), tf.float32))
-opt = tf.train.GradientDescentOptimizer(0.005)
-train_step = opt.minimize(error)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.005)
+train_step = optimizer.minimize(error)
 
 with tf.Session() as sess:
 
     sess.run(tf.global_variables_initializer())
 
-    for k in range(100000):
+    for k in range(10000):
 
         x_batch, y_batch = mnist.train.next_batch(100)
 
-        y_out, step, loss, accuracy = sess.run([y_, train_step, error, acc],
-                                               feed_dict={x: x_batch,
-                                                          y: y_batch})
-        if k % 1000 == 0:
-            print("Step", k, "Acc:", accuracy, 'Error:', loss)
-            # print(tf.reduce_sum(y_out[1]).eval())
+        step, e = sess.run([train_step, error], feed_dict={x: x_batch, y: y_batch})
+
+        if k % 100 == 0:
+            print("Step", k, 'Error:', e)
 
     accuracy = sess.run(acc, feed_dict={x: mnist.test.images,
                                         y: mnist.test.labels})
-    print("Final acc:", accuracy)
+    print("Test acc:", accuracy)
+
+
+
+
+
+
+
+
 
